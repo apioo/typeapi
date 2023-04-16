6ad42df63a90610dc4072966bcac06f2be55e76a
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-04-16 10:52:17 +0200

    remove faq

--- www/resources/template/inc/header.php
+++ www/resources/template/inc/header.php
@@ -34,9 +34,6 @@
     <li class="nav-item">
       <a class="nav-link" href="<?php echo $router->getAbsolutePath([\App\Controller\Generator::class, 'show']); ?>">Generator</a>
     </li>
-    <li class="nav-item">
-      <a class="nav-link" href="<?php echo $router->getAbsolutePath([\App\Controller\Faq::class, 'show']); ?>">FAQ</a>
-    </li>
   </ul>
   <a href="https://github.com/apioo/typeapi"><img src="<?php echo $base; ?>/img/github-32.png"></a>
 </nav>
