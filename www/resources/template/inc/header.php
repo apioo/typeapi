e6343cb54423cfba58ef79fe5f33f75455e1abb9
Christoph Kappestein <christoph.kappestein@gmail.com>
2023-05-13 11:41:34 +0200

    use min css and load js async

--- www/resources/template/inc/header.php
+++ www/resources/template/inc/header.php
@@ -3,21 +3,19 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="<?php echo $base; ?>/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo $base; ?>/css/highlight.min.css">
-  <link rel="stylesheet" href="<?php echo $base; ?>/css/app.css">
-  <link rel="canonical" href="<?php echo $router->getUrl($method); ?>">
-  <script src="<?php echo $base; ?>/js/highlight.min.js"></script>
-  <script src="<?php echo $base; ?>/js/jquery.min.js"></script>
-  <script src="<?php echo $base; ?>/js/popper.min.js"></script>
-  <script src="<?php echo $base; ?>/js/bootstrap.min.js"></script>
   <title>TypeAPI</title>
+  <link rel="preload" href="<?php echo $base; ?>/css/app.min.css" as="style" />
+  <link rel="stylesheet" href="<?php echo $base; ?>/css/app.min.css">
+  <link rel="canonical" href="<?php echo $router->getUrl($method); ?>">
+  <script async src="<?php echo $base; ?>/js/highlight.min.js"></script>
+  <script async src="<?php echo $base; ?>/js/jquery.min.js"></script>
+  <script async src="<?php echo $base; ?>/js/popper.min.js"></script>
+  <script async src="<?php echo $base; ?>/js/bootstrap.min.js"></script>
   <script async src="https://www.googletagmanager.com/gtag/js?id=G-8CL7811MFT"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
-
     gtag('config', 'G-8CL7811MFT', { 'anonymize_ip': true });
   </script>
 </head>
@@ -36,5 +34,5 @@
       <a class="nav-link" href="<?php echo $router->getAbsolutePath([\App\Controller\Generator::class, 'show']); ?>">Generator</a>
     </li>
   </ul>
-  <a href="https://github.com/apioo/typeapi"><img src="<?php echo $base; ?>/img/github-32.png"></a>
+  <a href="https://github.com/apioo/typeapi"><img src="<?php echo $base; ?>/img/github-32.png" width="32" alt="GitHub logo"></a>
 </nav>
