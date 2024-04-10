b3d4a5b39b0dea7d2b4ff0e1184bd0491627aacb
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-04-10 22:59:34 +0200

    optimize uses

--- www/resources/container.php
+++ www/resources/container.php
@@ -1,10 +1,7 @@
 <?php
 
 use PSX\Framework\Controller\ControllerInterface;
-use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 return static function (ContainerConfigurator $container) {
     $services = $container->services();
