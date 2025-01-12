d8828d4d9d910bb8cd99dc8151be508e2e4b9524
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-01-12 12:30:26 +0100

    remove has-background-white-bis class

--- www/resources/template/inc/footer.php
+++ www/resources/template/inc/footer.php
@@ -6,10 +6,10 @@
         <aside class="menu">
           <p class="menu-label">Links</p>
           <ul class="menu-list">
-            <li><a href="https://typeapi.org/" class="panel-block has-background-white-bis">TypeAPI</a></li>
-            <li><a href="https://typeschema.org/" class="panel-block has-background-white-bis">TypeSchema</a></li>
-            <li><a href="https://typehub.cloud/" class="panel-block has-background-white-bis">TypeHub</a></li>
-            <li><a href="https://sdkgen.app/" class="panel-block has-background-white-bis">SDKgen</a></li>
+            <li><a href="https://typeapi.org/" class="panel-block">TypeAPI</a></li>
+            <li><a href="https://typeschema.org/" class="panel-block">TypeSchema</a></li>
+            <li><a href="https://typehub.cloud/" class="panel-block">TypeHub</a></li>
+            <li><a href="https://sdkgen.app/" class="panel-block">SDKgen</a></li>
           </ul>
         </aside>
       </div>
@@ -17,9 +17,9 @@
         <aside class="menu">
           <p class="menu-label">GitHub</p>
           <ul class="menu-list">
-            <li><a href="https://github.com/apioo/typeapi" class="panel-block has-background-white-bis">TypeAPI</a></li>
-            <li><a href="https://github.com/apioo/typeschema" class="panel-block has-background-white-bis">TypeSchema</a></li>
-            <li><a href="https://github.com/apioo/sdkgen-cli" class="panel-block has-background-white-bis">SDKgen CLI</a></li>
+            <li><a href="https://github.com/apioo/typeapi" class="panel-block">TypeAPI</a></li>
+            <li><a href="https://github.com/apioo/typeschema" class="panel-block">TypeSchema</a></li>
+            <li><a href="https://github.com/apioo/sdkgen-cli" class="panel-block">SDKgen CLI</a></li>
           </ul>
         </aside>
       </div>
@@ -27,10 +27,10 @@
         <aside class="menu">
           <p class="menu-label">Contact</p>
           <ul class="menu-list">
-            <li><a href="https://discord.gg/eMrMgwsc6e" class="panel-block has-background-white-bis">Discord</a></li>
-            <li><a href="https://twitter.com/ApiooTech" class="panel-block has-background-white-bis">Twitter</a></li>
-            <li><a href="https://www.apioo.de/en/contact" class="panel-block has-background-white-bis">Contact</a></li>
-            <li><a href="https://www.apioo.de/en/imprint" class="panel-block has-background-white-bis">Imprint</a></li>
+            <li><a href="https://discord.gg/eMrMgwsc6e" class="panel-block">Discord</a></li>
+            <li><a href="https://twitter.com/ApiooTech" class="panel-block">Twitter</a></li>
+            <li><a href="https://www.apioo.de/en/contact" class="panel-block">Contact</a></li>
+            <li><a href="https://www.apioo.de/en/imprint" class="panel-block">Imprint</a></li>
           </ul>
         </aside>
       </div>
