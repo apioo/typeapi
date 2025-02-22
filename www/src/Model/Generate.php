dd7caee9ba50a15b84f7fe566d7f6b2178a363b8
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-02-22 23:15:17 +0100

    update gen

--- www/src/Model/Generate.php
+++ www/src/Model/Generate.php
@@ -8,44 +8,44 @@ use PSX\Schema\Attribute\Key;
 
 class Generate implements \JsonSerializable, \PSX\Record\RecordableInterface
 {
-    protected ?string $namespace = null;
+    protected ?string $type = null;
     protected ?string $schema = null;
     #[Key('g-recaptcha-response')]
     protected ?string $gRecaptchaResponse = null;
-    public function setNamespace(?string $namespace) : void
+    public function setType(?string $type): void
     {
-        $this->namespace = $namespace;
+        $this->type = $type;
     }
-    public function getNamespace() : ?string
+    public function getType(): ?string
     {
-        return $this->namespace;
+        return $this->type;
     }
-    public function setSchema(?string $schema) : void
+    public function setSchema(?string $schema): void
     {
         $this->schema = $schema;
     }
-    public function getSchema() : ?string
+    public function getSchema(): ?string
     {
         return $this->schema;
     }
-    public function setGRecaptchaResponse(?string $gRecaptchaResponse) : void
+    public function setGRecaptchaResponse(?string $gRecaptchaResponse): void
     {
         $this->gRecaptchaResponse = $gRecaptchaResponse;
     }
-    public function getGRecaptchaResponse() : ?string
+    public function getGRecaptchaResponse(): ?string
     {
         return $this->gRecaptchaResponse;
     }
-    public function toRecord() : \PSX\Record\RecordInterface
+    public function toRecord(): \PSX\Record\RecordInterface
     {
         /** @var \PSX\Record\Record<mixed> $record */
         $record = new \PSX\Record\Record();
-        $record->put('namespace', $this->namespace);
+        $record->put('type', $this->type);
         $record->put('schema', $this->schema);
         $record->put('g-recaptcha-response', $this->gRecaptchaResponse);
         return $record;
     }
-    public function jsonSerialize() : object
+    public function jsonSerialize(): object
     {
         return (object) $this->toRecord()->getAll();
     }
